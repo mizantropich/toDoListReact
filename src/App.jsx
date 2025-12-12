3ab9f52cbde506ed88dd7d6d11e851e221974aa1
mizantropich <soldatov.nickolai2018@yandex.ru>
2025-12-12 16:32:43 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import { useState } from 'react';
+import styles from './App.module.css';
+
+function App() {
+	return (
+		<div className={styles.app}>
+			<h1 className={styles.title}>📝 Todo List</h1>
+		</div>
+	);
+}
+
+export default App;
